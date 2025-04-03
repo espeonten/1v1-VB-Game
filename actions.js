9783129a20eb9d0ff187975bd89a93cdcb417670
espeonten <sydenteo@gmail.com>
2025-04-03 11:33:17 -0700

    Update actions.js
    added killer spike

--- actions.js
+++ actions.js
@@ -7,17 +7,37 @@ function action(sprite, sprite2){
         }
     }
     else if(pAction == "serve"){
-        ball.velocityX= 1
-        ball.velocityY= -15
+        if(keyDown("right")){
+            ball.velocityX = 2
+            ball.velocityY = -15
+        }
+        else{
+            ball.velocityX = 0
+            ball.velocityY = -15
+        }
         pAction = "play"
         lastPersonWhoHitTheBall = "p"
     }
     else if(pAction == "play"){
             if(p.isTouching(ball)){
-                if(keyDown("space")){
+                if(keyDown("shift")){
+                    if(keyDown("space") && keyDown("up")){
+                        p.changeAnimation("swing")
+                        tooHard = true
+                        ball.velocityX= 90
+                        ball.velocityY = 18
+                        lastPersonWhoHitTheBall = "p"
+                    }
+                    else{
+                        ball.velocityX= 0
+                        ball.velocityY= -6
+                    }
+                }
+                else if(keyDown("space")){
+                    p.changeAnimation("swing")
                     if(p.x > sprite.x){
                         ball.velocityX= 40
-                        ball.velocityY = 23.5
+                        ball.velocityY = 30
                         lastPersonWhoHitTheBall = "p"
                     }
                     else if(p.x < sprite.x && p.x > sprite2.x){
@@ -31,10 +51,6 @@ function action(sprite, sprite2){
                         lastPersonWhoHitTheBall = "p"
                     }
                 }
-                else if(keyDown("shift")){
-                    ball.velocityX= 0
-                    ball.velocityY= -6
-                }
                 else if(keyDown("down")){
                     ball.velocityX= 10
                     ball.velocityY= -15
@@ -42,13 +58,21 @@ function action(sprite, sprite2){
                 }
                 else if(keyDown("z")){
                     if(keyDown("right")){
-                        ball.velocityX = 5
+                        if(keyDown("up")){
+                            ball.velocityX = 5
+                            ball.velocityY = -10
+                        }
+                        ball.velocityX = 3
                         ball.velocityY = -10
                     }
                     else{
                         ball.velocityX = 0.5
                         ball.velocityY = -10
                     }
+                }
+                else if(keyDown("x")){
+                    ball.velocityX= 40
+                    ball.velocityY= 60
                 }
             }
         }
